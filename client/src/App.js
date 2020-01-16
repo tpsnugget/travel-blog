a57d577899467bbd8a32ab836342b498951cb4dc
Michael Giebner <mikegiebner@gmail.com>
2020-01-15 19:00:52 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App is up Man!</h1>
+    </div>
+  );
+}
+
+export default App;
